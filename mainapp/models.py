afb5ac56142df485babd1d72b10b67b5877d35a0
TsAndriy <a.andriy.t@gmail.com>
2021-07-13 13:20:31 +0300

    django manager + template

--- mainapp/models.py
+++ mainapp/models.py
@@ -11,7 +11,10 @@ from io import BytesIO
 
 User=get_user_model()
 
-def get_product_url(obj, viewname,):
+def get_models_for_count(*model_names):
+    return [models.Count(model_name) for model_name in model_names]
+
+def get_product_url(obj, viewname,): 
     ct_model = obj.__class__._meta.model_name
     return reverse(viewname, kwargs={'ct_model': ct_model, 'slug': obj.slug})
 
@@ -46,9 +49,25 @@ class LatestProducts:
 
     objects=LatestProductsManager()
 
+class CategoryManager(models.Manager):
+
+    CATEGORY_NAME_COUNT_NAME={
+        'Notebooks': 'notebook__count',
+        'Smartphones': 'smartphone__count'
+    }
+
+    def get_queryset(self):
+        return super().get_queryset()
+    
+    def get_categories_for_left_sidebar(self):
+        models = get_models_for_count('notebook', 'smartphone')
+        qs = list(self.get_queryset().annotate(*models).values())
+        return [dict(name=c['name'], slug=c['slug'], count=c[self.CATEGORY_NAME_COUNT_NAME[c['name']]]) for c in qs]
+
 class Category(models.Model):
     name=models.CharField(max_length=255, verbose_name='Name category')
     slug=models.SlugField(unique=True)
+    objects = CategoryManager()
 
     def __str__(self):
         return self.name
