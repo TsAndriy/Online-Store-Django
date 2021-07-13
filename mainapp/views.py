97447a30dfa450bffcf057ba96083f55f003959f
TsAndriy <a.andriy.t@gmail.com>
2021-07-13 16:42:39 +0300

    Mixins + Products in Home Page

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
-from .models import Notebook, Smartphone, Category
+from django.views.generic import DetailView, View
 
-def test_view(request):
-    categories = Category.objects.get_categories_for_left_sidebar()
-    return render(request, 'base.html', {'categories': categories})
+from .models import Notebook, Smartphone, Category, LatestProducts
+from .mixins import CategoryDetailMixin
 
-class ProductDetailView(DetailView):
+class BaseViev(View):
+
+    def get(self, request, *args, **kwargs):
+        categories = Category.objects.get_categories_for_left_sidebar()
+        products = LatestProducts.objects.get_products_for_main_page(
+            'notebook', 'smartphone', with_respect_to='smartphone'
+        )
+        context = {
+            'categories': categories,
+            'products': products
+        }
+        return render(request, 'base.html', context)
+
+class ProductDetailView(CategoryDetailMixin, DetailView):
 
     CT_MODEL_MODEL_CLASS = {
         'notebook': Notebook,
@@ -21,4 +32,11 @@ class ProductDetailView(DetailView):
     context_object_name='product'
     template_name='product_detail.html'
     slug_url_kwarg='slug'
-     
+
+class CategoryDetailViev(CategoryDetailMixin, DetailView):
+
+    model = Category
+    queryset = Category.objects.all()
+    context_object_name = 'category'
+    template_name = 'category_detail.html'
+    slug_url_kwarg = 'slug'
